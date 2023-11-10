e3c6df6e0ba15df9cbf011583c81a8844e3be124
Tyler <m0bley@icloud.com>
2023-11-10 12:11:30 +0000

    Under construction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonDigging, faToolbox } from "@fortawesome/free-solid-svg-icons";
+
+import "./Reset.css";
 import "./App.css";
 
 function App() {
     return (
         <>
-            <h1>Dark Frontier</h1>
+            <div className="body-wrap">
+                <div className="hazard-background"></div>
+                <div>
+                    <h1>
+                        <FontAwesomeIcon icon={faToolbox} bounce />
+                        !UNDER CONSTRUCTION!
+                        <FontAwesomeIcon icon={faPersonDigging} shake />
+                    </h1>
+                </div>
+
+                <div className="hazard-background"></div>
+            </div>
         </>
     );
 }

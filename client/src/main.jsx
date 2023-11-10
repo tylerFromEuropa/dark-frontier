b7ea2f564cd8c1eaa4d2215fb921a9a3b0dc26c0
Tyler <m0bley@icloud.com>
2023-11-10 15:03:52 +0000

    Login page created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Auth0Provider
+            domain="tyler.uk.auth0.com"
+            clientId="FTpq1OocH8JJP5gCP8ESHsjIv10euuu2"
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+        >
+            <App />
+        </Auth0Provider>
     </React.StrictMode>
 );
---
+++ client/src/pages/Start.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "../App.css";
+import LoginButton from "../components/LoginButton";
+import Profile from "../components/Profile";
+
+export default function Start() {
+    return (
+        <>
+            <LoginButton />
+        </>
+    );
+}

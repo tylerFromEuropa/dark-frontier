a69adc25b83f138ab8595c1d8a2f0a67a289c1ec
Tobias Foit <tobias.foit@gmail.com>
2023-11-12 14:06:49 +0000

    char in header

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "../CSS/Game.css";
 import Header from "../components/Header";
 import Characterselection from "../components/Characterselection";
+import characters from "../assets/characters.json";
 
 export default function Game() {
   const [currentStep, setCurrentStep] = useState(0);
   const [character, setCharacter] = useState(0);
   return (
     <>
-      <Header />
+      <Header characters={characters} character={character} />
       <div id="gamecontainer">
         {currentStep === 0 && (
           <Characterselection

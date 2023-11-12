afe2db5572c3075c07a5710c5ff7f73a3c682ff5
Tyler <m0bley@icloud.com>
2023-11-12 09:25:50 +0000

    Styling changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,33 @@ import { useState } from "react";
 import "./CSS/Reset.css";
 import "./CSS/App.css";
 import Profile from "./components/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Header from "./components/Header";
 
+import Game from "./pages/Game";
+import Menu from "./components/Menu";
+import Construction from "./components/Construction";
+import Start from "./pages/Start";
+
 function App() {
-  const { isAuthenticated } = useAuth0();
-  return (
-    <>
-      <Header />
-    </>
-  );
+    const { isAuthenticated } = useAuth0();
+    return (
+        <>
+            {/* <Header /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/menu" element={<Menu />}></Route>
+                    <Route path="/" element={<Start />}></Route>
+                    <Route
+                        path="/game"
+                        // element={isAuthenticated ? <Game /> : <Start />}
+                        element={<Game />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;

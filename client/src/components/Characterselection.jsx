587af6d1dd4278a591e26c46417e867f231d6359
Tobias Foit <tobias.foit@gmail.com>
2023-11-12 11:20:02 +0000

    just some more css

--- client/src/components/Characterselection.jsx
+++ client/src/components/Characterselection.jsx
@@ -8,27 +8,36 @@ export default function Characterselection({ setCurrentStep, setCharacter }) {
     <div id="charselectcontainer">
       <h2>Select your adventurer</h2>
       <div id="charselect">
-        <img
-          src={zarnok}
-          alt="zarnok"
-          onClick={() => {
-            setCharacter(0), setCurrentStep(1);
-          }}
-        />
-        <img
-          src={xylophus}
-          alt="xylophus"
-          onClick={() => {
-            setCharacter(1), setCurrentStep(1);
-          }}
-        />
-        <img
-          src={quasarax}
-          alt="quasarax"
-          onClick={() => {
-            setCharacter(2), setCurrentStep(1);
-          }}
-        />
+        <div className="character">
+          <img
+            src={zarnok}
+            alt="zarnok"
+            onClick={() => {
+              setCharacter(0), setCurrentStep(1);
+            }}
+          />
+          <h3>Zarnok</h3>
+        </div>
+        <div className="character">
+          <img
+            src={xylophus}
+            alt="xylophus"
+            onClick={() => {
+              setCharacter(1), setCurrentStep(1);
+            }}
+          />{" "}
+          <h3>Xylophus</h3>
+        </div>
+        <div className="character">
+          <img
+            src={quasarax}
+            alt="quasarax"
+            onClick={() => {
+              setCharacter(2), setCurrentStep(1);
+            }}
+          />{" "}
+          <h3>Quasarax</h3>
+        </div>
       </div>
     </div>
   );

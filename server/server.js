ba902bd88e0f14f7e0ea16bf87a66f6b252a509f
liam02walker <17liamwalker02@gmail.com>
2023-11-14 18:30:46 +0000

    fix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const Users = require("./models/user");
 mongoose.connect(process.env.MONGODB_LINK);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  const allowedOrigins = ["http://localhost:5173", "https://dark-frontier.vercel.app"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });

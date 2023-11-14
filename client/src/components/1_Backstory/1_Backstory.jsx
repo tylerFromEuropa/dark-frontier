cccccd12955aa9c2f524190c493144b0f5f235c0
Tyler <m0bley@icloud.com>
2023-11-14 14:10:24 +0000

    more chnages

---
+++ client/src/components/1_Backstory/1_Backstory.jsx
@@ -0,0 +1,8 @@
+export default function $1_Backstory() {
+    return (
+        <section className="backstory-page">
+            <section className="background"></section>
+            <section className="selections"></section>
+        </section>
+    );
+}

a4c2d0d4f0ba22775b235873b6983646062a67a6
Tyler <m0bley@icloud.com>
2023-11-14 13:32:40 +0000

    Styling edits

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -3,15 +3,18 @@ import "../CSS/Start.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
-  return (
-    <div className="loginContainer">
-      <button className="loginButton" onClick={() => loginWithRedirect()}>
-        LOG IN / SIGN UP
-      </button>
-    </div>
-  );
+    return (
+        <div className="loginContainer">
+            <p>
+                Welcome to <span id="df-login">Dark Frontier</span>
+            </p>
+            <button className="loginButton" onClick={() => loginWithRedirect()}>
+                LOG IN / SIGN UP
+            </button>
+        </div>
+    );
 };
 
 export default LoginButton;

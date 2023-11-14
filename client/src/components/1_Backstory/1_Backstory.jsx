16c43ffa31c6fca41dea8021cf85224d7125aa1a
Samuel Draper <samueledraper@outlook.com>
2023-11-14 15:44:24 +0000

    add story choice functionality

--- client/src/components/1_Backstory/1_Backstory.jsx
+++ client/src/components/1_Backstory/1_Backstory.jsx
@@ -1,8 +1,58 @@
+import { useState } from "react";
+import "./1_Backstory.css";
+import storyline from "./Backstory.json";
+
 export default function $1_Backstory() {
-    return (
-        <section className="backstory-page">
-            <section className="background"></section>
-            <section className="selections"></section>
-        </section>
-    );
+  const [storyChoice, setStoryChoice] = useState(0);
+  const [optionCheck, setOptionCheck] = useState(false);
+
+  function checkingStory() {
+    if (storyChoice === 1) {
+      setOptionCheck(true);
+    } else {
+      setOptionCheck(false);
+    }
+    console.log(optionCheck, storyChoice);
+  }
+
+  function findWater() {
+    const randomNumber = Math.random();
+    if (randomNumber <= 0.5) {
+      setStoryChoice(3);
+    } else {
+      setStoryChoice(2);
+    }
+    checkingStory();
+  }
+
+  return (
+    <section className="backstory-page">
+      <section className="background">
+        <img src="" alt="" />
+        <p>{storyline[storyChoice]}</p>
+      </section>
+      <section className="selections">
+        {!optionCheck && (
+          <div>
+            <button
+              onClick={() => {
+                setStoryChoice(storyChoice + 1);
+                checkingStory();
+              }}
+            >
+              Continue
+            </button>
+          </div>
+        )}
+
+        {optionCheck && (
+          <div>
+            {storyChoice === 1 && (
+              <button onClick={findWater()}>Look around the lifepod ship for some water</button>
+            )}
+          </div>
+        )}
+      </section>
+    </section>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,29 @@ import "../CSS/Header.css";
 import Menu from "./Menu";
 import characters from "../JSON/characters.json";
 
-export default function Header({ myCharacter }) {
-    const [menuStatus, setMenuStatus] = useState(false);
-    return (
-        <div className="headerContainer">
-            <h1>DARK FRONTIER</h1>
-            <div onClick={() => setMenuStatus(!menuStatus)} className={`menuopen${menuStatus}`}>
-                {menuStatus && <Menu />}
-            </div>
-        </div>
-    );
+export default function Header({ myCharacter, myUser, setMyUser }) {
+  const [menuStatus, setMenuStatus] = useState(false);
+
+  const healthDivs = [];
+
+  for (let i = 0; i < myUser.character.health; i++) {
+    healthDivs.push(<div key={i} className="health-bar-segment"></div>);
+  }
+
+  return (
+    <div className="headerContainer">
+      {myUser.myID !== "" && (
+        <section>
+          <img src="" alt="" />
+          <div>
+            <h2>{myUser.character.name}</h2>
+            <div className="health-bar">{healthDivs}</div>
+          </div>
+        </section>
+      )}
+      <div onClick={() => setMenuStatus(!menuStatus)} className={`menuopen${menuStatus}`}>
+        {menuStatus && <Menu />}
+      </div>
+    </div>
+  );
 }

49735aaeaba3775405718ddc3c4ddbedb30ec735
Tyler <m0bley@icloud.com>
2023-11-15 15:16:46 +0000

    Added route to about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Game from "./pages/Game";
 import Start from "./pages/Start";
+import About from "./pages/About";
 
 function App() {
     const { isAuthenticated } = useAuth0();
@@ -17,6 +18,7 @@ function App() {
                         element={isAuthenticated ? <Game /> : <Start />}
                         // element={<Game />}
                     ></Route>
+                    <Route path="/about" element={<About />}></Route>
                 </Routes>
             </BrowserRouter>
         </>

fdaf76b5c8700e751c70ac57791771f950994a5b
liam02walker <17liamwalker02@gmail.com>
2023-11-15 12:28:59 +0000

    server side issue

--- server/server.js
+++ server/server.js
@@ -11,6 +11,25 @@ const Users = require("./models/user");
 
 mongoose.connect(process.env.MONGODB_LINK);
 
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:5173", "https://dark-frontier.vercel.app"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.get("/", (_, response) => {
   response.json("These are not the GETs you're looking for.");
 });

eb1c672088f6fc2911cacc5365b614a86374ea0f
liam02walker <17liamwalker02@gmail.com>
2023-11-16 10:52:25 +0000

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Demo App",
+    short_name: "De A",
+    description: "An app that is a demo",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable-icon.png",
+        sizes: "255x255",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#ffe207",
+    background_color: "#1b1b1b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "landscape",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});

b3d1873c0a9d68f693d84616225fdbc12959a0bf
Samuel Draper <samueledraper@outlook.com>
2023-11-16 11:50:47 +0000

    card game CSS

--- client/src/components/10_Minigames/10_CardGame.jsx
+++ client/src/components/10_Minigames/10_CardGame.jsx
@@ -1,96 +1,106 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./10_CardGame.css";
 
 export default function $10_CardGame({ setCurrentStep }) {
-    const [cardDeck, setCardDeck] = useState();
+  const [cardDeck, setCardDeck] = useState();
 
-    const [dealerCard, setdealerCard] = useState();
-    const [userCard, setUserCard] = useState();
+  const [dealerCard, setdealerCard] = useState();
+  const [userCard, setUserCard] = useState();
 
-    const [userGuess, setUserGuess] = useState("");
-    const [userGuessButton, setUserGuessButton] = useState(false);
+  const [userGuess, setUserGuess] = useState("");
+  const [userGuessButton, setUserGuessButton] = useState(false);
 
-    const [userCardButton, setuserCardButton] = useState(false);
+  const [userCardButton, setuserCardButton] = useState(false);
 
-    const [result, setResult] = useState("");
+  const [result, setResult] = useState("");
 
-    useEffect(() => {
-        getCardDeck();
-    }, []);
+  useEffect(() => {
+    getCardDeck();
+  }, []);
 
-    async function getCardDeck() {
-        const API = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
-        const res = await axios.get(API);
-        setCardDeck(res.data);
+  async function getCardDeck() {
+    const API = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+    const res = await axios.get(API);
+    setCardDeck(res.data);
+  }
+
+  async function getDealerCard() {
+    const API = `https://www.deckofcardsapi.com/api/deck/${cardDeck.deck_id}/draw/?count=1`;
+    const res = await axios.get(API);
+    setdealerCard(res.data);
+    setUserGuessButton(!userGuessButton);
+  }
+
+  async function getUserCard() {
+    const API = `https://www.deckofcardsapi.com/api/deck/${cardDeck.deck_id}/draw/?count=1`;
+    const res = await axios.get(API);
+    setUserCard(res.data);
+    setuserCardButton(!userCardButton);
+  }
+
+  function setHigherGuess() {
+    setUserGuess("higher");
+    setuserCardButton(!userCardButton);
+    setUserGuessButton(!userGuessButton);
+  }
+
+  function setLowerGuess() {
+    setUserGuess("lower");
+    setuserCardButton(!userCardButton);
+    setUserGuessButton(!userGuessButton);
+  }
+
+  function checkResult() {
+    if (dealerCard.cards[0].value > userCard.cards[0].value && userGuess === "higher") {
+      setResult("lose");
+      setCurrentStep(11);
+    } else if (dealerCard.cards[0].value > userCard.cards[0].value && userGuess === "lower") {
+      setResult("win");
+      setCurrentStep(96);
+    } else if (dealerCard.cards[0].value < userCard.cards[0].value && userGuess === "lower") {
+      setResult("lose");
+      setCurrentStep(11);
+    } else if (dealerCard.cards[0].value < userCard.cards[0].value && userGuess === "higher") {
+      setResult("win");
+      setCurrentStep(96);
+    } else {
+      setResult("tie");
+      setCurrentStep(11);
     }
+  }
 
-    async function getDealerCard() {
-        const API = `https://www.deckofcardsapi.com/api/deck/${cardDeck.deck_id}/draw/?count=1`;
-        const res = await axios.get(API);
-        setdealerCard(res.data);
-        setUserGuessButton(!userGuessButton);
-    }
+  return (
+    <section className="card-game">
+      <div className="cards">
+        {!dealerCard ? (
+          <img src="https://www.deckofcardsapi.com/static/img/back.png" />
+        ) : (
+          <img src={dealerCard.cards[0].image} />
+        )}
+        {!userCard ? (
+          <img src="https://www.deckofcardsapi.com/static/img/back.png" />
+        ) : (
+          <img src={userCard.cards[0].image} />
+        )}
+      </div>
 
-    async function getUserCard() {
-        const API = `https://www.deckofcardsapi.com/api/deck/${cardDeck.deck_id}/draw/?count=1`;
-        const res = await axios.get(API);
-        setUserCard(res.data);
-        setuserCardButton(!userCardButton);
-    }
+      <div className="options">
+        {userGuessButton && (
+          <>
+            <p>Make your guess...</p>
+            <button onClick={setHigherGuess}>Higher</button>
+            <button onClick={setLowerGuess}>Lower</button>
+          </>
+        )}
 
-    function setHigherGuess() {
-        setUserGuess("higher");
-        setuserCardButton(!userCardButton);
-        setUserGuessButton(!userGuessButton);
-    }
+        {!dealerCard && <button onClick={getDealerCard}>Start</button>}
 
-    function setLowerGuess() {
-        setUserGuess("lower");
-        setuserCardButton(!userCardButton);
-        setUserGuessButton(!userGuessButton);
-    }
+        {userCardButton && <button onClick={getUserCard}>Reveal Dealer's Card</button>}
 
-    function checkResult() {
-        if (dealerCard.cards[0].value > userCard.cards[0].value && userGuess === "higher") {
-            setResult("lose");
-            setCurrentStep(11);
-        } else if (dealerCard.cards[0].value > userCard.cards[0].value && userGuess === "lower") {
-            setResult("win");
-            setCurrentStep(96);
-        } else if (dealerCard.cards[0].value < userCard.cards[0].value && userGuess === "lower") {
-            setResult("lose");
-            setCurrentStep(11);
-        } else if (dealerCard.cards[0].value < userCard.cards[0].value && userGuess === "higher") {
-            setResult("win");
-            setCurrentStep(96);
-        } else {
-            setResult("tie");
-            setCurrentStep(11);
-        }
-    }
-
-    return (
-        <>
-            <div className="cards">
-                {!dealerCard ? <img src="https://www.deckofcardsapi.com/static/img/back.png" /> : <img src={dealerCard.cards[0].image} />}
-                {!userCard ? <img src="https://www.deckofcardsapi.com/static/img/back.png" /> : <img src={userCard.cards[0].image} />}
-            </div>
-
-            <div className="options">
-                {userGuessButton && (
-                    <>
-                        <button onClick={setHigherGuess}>Higher</button>
-                        <button onClick={setLowerGuess}>Lower</button>
-                    </>
-                )}
-
-                {!dealerCard && <button onClick={getDealerCard}>Start</button>}
-
-                {userCardButton && <button onClick={getUserCard}>Reveal Card Two</button>}
-
-                {dealerCard && userCard && <button onClick={checkResult}>check result</button>}
-            </div>
-            {<p>{result}</p>}
-        </>
-    );
+        {dealerCard && userCard && <button onClick={checkResult}>check result</button>}
+      </div>
+      {<p>{result}</p>}
+    </section>
+  );
 }

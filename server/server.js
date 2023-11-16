7753a1bfc2f9c43b78261dd8cdeef5e0b487b6b9
liam02walker <17liamwalker02@gmail.com>
2023-11-16 11:40:00 +0000

    plz

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-app.use(express.json());
 app.use(cors);
+app.use(express.json());
 const PORT = 8080;
 
 const mongoose = require("mongoose");
